ced4a0ec16da533be07cb9a4c7e30b7406f78b2a
tilde-humleklo-nti-johanneberg <tilde.humleklo@elev.ga.ntig.se>
2022-03-14 10:09:56 +0100

    hamburgare

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Denna fil skall innehålla JavaScript (JS)
+   const nav = document.querySelector('.navLinks')
+   const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+nav.classList.toggle('navActive')
+burger.classList.toggle('toggle')
+}
+
+burger.addEventListener('click', toggleMeny)
